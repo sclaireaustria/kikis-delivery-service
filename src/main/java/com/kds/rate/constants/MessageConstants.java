6aae73f582070735d6ccc0321460df84c3539c3e
sclaireaustria <sclaireaustria@gmail.com>
2023-01-31 19:05:51 +0800

    Updated response message

--- src/main/java/com/kds/rate/constants/MessageConstants.java
+++ src/main/java/com/kds/rate/constants/MessageConstants.java
@@ -2,9 +2,9 @@ package com.kds.rate.constants;
 
 public class MessageConstants {
 
-	public static final String VALID_ORDER = "Order can be processed.";
-	public static final String INVALID_ORDER = "Order cannot be processed.";
+	public static final String VALID_ORDER = "Nice, Kiki can deliver your package.";
+	public static final String INVALID_ORDER = "Oops! Your package is too heavy for Kiki.";
 	
-	public static final String VALID_VOUCHER = "Discount applied:";
-	public static final String INVALID_VOUCHER = "Invalid voucher, no discount applied.";
+	public static final String VALID_VOUCHER = "And you got a discount worth";
+	public static final String INVALID_VOUCHER = "Sorry, your voucher is invalid.";
 }
