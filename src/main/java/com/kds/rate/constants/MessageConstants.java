c076f1161fb36f636ad5a1dc792b32d8107f3928
sclaireaustria <sclaireaustria@gmail.com>
2023-01-31 16:31:36 +0800

    Removed space from discount message

--- src/main/java/com/kds/rate/constants/MessageConstants.java
+++ src/main/java/com/kds/rate/constants/MessageConstants.java
@@ -5,6 +5,6 @@ public class MessageConstants {
 	public static final String VALID_ORDER = "Order can be processed.";
 	public static final String INVALID_ORDER = "Order cannot be processed.";
 	
-	public static final String VALID_VOUCHER = "Discount applied: ";
+	public static final String VALID_VOUCHER = "Discount applied:";
 	public static final String INVALID_VOUCHER = "Invalid voucher, no discount applied.";
 }
